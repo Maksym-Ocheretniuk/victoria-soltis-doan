271eb8cc7bdb6f330c894aecfd80a57eee401e62
Maksym Ocheretniuk <max.ocheret@gmail.com>
2023-04-24 18:31:05 +0300

    cteate "My illustrations"

---
+++ js/books-gallery-items.js
@@ -0,0 +1,12 @@
+export const booksGalleryItems = [
+	{
+		preview: "./images/illustrations/Illustration-01_preview.jpg",
+		original: "./images/illustrations/Illustration-01.jpg",
+		description: "Illustration for the Wimmelbook ''Transport'', PET publishing house",
+	},
+	{
+		preview: "./images/illustrations/Illustration-02_preview.jpg",
+		original: "./images/illustrations/Illustration-02.jpg",
+		description: "Illustration for the annual calendar for the Raduga advertising agenc",
+	},
+];
